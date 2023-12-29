d6f01de6e6d7c122b5210393ae6a927aa91fae25
Vishal kumar <vishalk80269@gmail.com>
2023-12-29 17:52:49 +0530

    work on responsive

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaHandHoldingMedical } from "react-icons/fa6";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useEffect } from "react";
 import profile from "../Images/doctorAbout.jpg";
 // Import Swiper styles
 import "swiper/css";
@@ -10,11 +11,36 @@ import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
 const Doctor = () => {
+  const [slidesPerView, setSlidesPerView] = useState(5);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 525) {
+        console.log("working");
+        setSlidesPerView(1);
+      } else if (window.innerWidth <= 991) {
+        setSlidesPerView(3);
+      } else {
+        setSlidesPerView(5);
+      }
+    };
+
+    // Initial setup
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="doctor">
-      <div className="container">
-        <div className="row py-5">
-          <div className="row aboutHeading">
+      <div className="container ">
+        <div className="row py-5 ">
+          <div className="row aboutHeading  m-0">
             <h5>
               <span>
                 <FaHandHoldingMedical fontSize={25} />
@@ -22,12 +48,12 @@ const Doctor = () => {
               <span>Our Doctors</span>
             </h5>
           </div>
-          <div className="row aboutSubheading">
+          <div className="row aboutSubheading  m-0">
             <h1>Our Best Doctor</h1>
           </div>
-          <div className="row slider ">
+          <div className="row slider  m-0">
             <Swiper
-              slidesPerView={5}
+              slidesPerView={slidesPerView}
               centeredSlides={false}
               spaceBetween={10}
               navigation={true}
