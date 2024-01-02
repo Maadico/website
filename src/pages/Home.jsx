0059d8f6eb85c130b2644f566fe7e1bb00a08c5c
Vishal kumar <vishalk80269@gmail.com>
2024-01-02 23:21:00 +0530

    remove some fields from contact  from

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,11 @@ import Facilities from "../components/Facilities";
 import Doctor from "../components/Doctor";
 import Review from "../components/Review";
 import Footer from "../components/Footer";
+import { MdAddIcCall } from "react-icons/md";
+import { BiSolidToTop } from "react-icons/bi";
 const Home = () => {
   return (
-    <div>
+    <div id="heroSection">
       <Hero />
       <HeroTwo />
       <About />
@@ -22,6 +24,18 @@ const Home = () => {
       <Facilities />
       <Doctor />
       <Footer />
+      <div className="fixIcon">
+        <div className="icon1">
+          <a href="">
+            <MdAddIcCall fontSize={25} color="white" />
+          </a>
+        </div>
+        <div className="icon1">
+          <a href="#heroSection">
+            <BiSolidToTop fontSize={25} color="white" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
