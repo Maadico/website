196613d38dca2c3f29adf5caa051e0ef86f5835a
Vishal kumar <vishalk80269@gmail.com>
2024-05-21 15:06:08 +0530

    add refund policy

---
+++ src/pages/rule/ShippingPolicy.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ShippingPolicy = () => {
+  return (
+    <div>
+      <div className="container">
+        <section class="mt-4">
+          <h4>Shipping Policy</h4>
+          <p>
+            We charge a flat shipping charge per order irrespective of the order
+            size and value. Flat rates apply and the customers can choose from
+            the below options at the time of checkout:
+          </p>
+          <p>
+            <strong>Standard Shipping: Rs. 50</strong>
+          </p>
+          <p>
+            The orders are shipped within 6-7 working days from the date it is
+            received. Shipping details will be shared on the registered mobile
+            number and Email ID (if shared).
+          </p>
+          <p>
+            Cancellation cannot be requested once an order is shipped out from
+            our warehouse.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default ShippingPolicy;
