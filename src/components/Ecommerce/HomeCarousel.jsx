19c02972a94f79cdddd06092c8ddc0922dd298a6
Vishal kumar <vishalk80269@gmail.com>
2024-09-19 00:38:05 +0530

    fix:image slider

--- src/components/Ecommerce/HomeCarousel.jsx
+++ src/components/Ecommerce/HomeCarousel.jsx
@@ -61,22 +61,32 @@ function HomeCarousel() {
           />
         </div> */}
       </div>
-      {/* <a
-        className="carousel-control-prev text-primary"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      </a> */}
+      {slider.length > 0 && (
+        <>
+          <a
+            className="carousel-control-prev text-primary"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+          </a>
+        </>
+      )}
     </div>
   );
 }
